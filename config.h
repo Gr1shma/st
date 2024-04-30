851cdfcd4bd218164f447190ba2e4f363e31bc41
Grishma Dhakal <dhakalgrishma5@gmail.com>
2024-04-30 14:34:25 +0545

    changes for predator

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrainsMono:pixelsize=22:antialias=true:autohint=true";
 // static char *font2[] = { "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
-static char *font2[] = { "FontAwesome:size=14:antialias=true:autohint=true" };
+static char *font2[] = { "FontAwesome:size=22:antialias=true:autohint=true" };
 static int borderpx = 15;
 
 /*
